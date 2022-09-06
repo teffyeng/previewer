3ec148c9dd865ea864c7db06d3e2fbb049ab6a80
Teffy English <teffy.english@gmail.com>
2022-09-06 12:52:48 -0500

    deployment

--- react-table/src/App.test.js
+++ react-table/src/App.test.js
@@ -1,7 +1,5 @@
-// eslint-disable-next-line 
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
 
 test('renders learn react link', () => {
   render(<App />);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import  { marked } from 'marked';
 import './App.css';
+import React from 'react';
+
+
+
 
 function App() {
+  
+  const [text, setText] = React.useState(`
+  # H1
+  ## H2
+  [title](https://www.example.com)
+  \`code\`
+  \`\`\`
+  {
+  "firstName": "Jan",
+  "lastName":"Johnson",
+  "age": 25
+  }
+
+ \`\`\`
+ - First item
+ - Second item
+ - Third item
+ > blockquote
+
+ ![alt text](image.jpg)
+
+ **bold text**
+
+ `);
+
+ marked.setOptions({
+  breaks: true
+ })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<div className="App">
+  
+      <textarea className='editor'
+      id="editor" 
+      onChange ={(event) => {
+      setText(event.target.value);
+    }} value={text}></textarea>
+    <div className="preview" id="preview" 
+    dangerouslySetInnerHTML={{
+      __html: marked(text),
+    }}
+    ></div>
+  </div>
+    
   );
 }
 
